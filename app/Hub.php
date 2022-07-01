14e5b29500bfef34426550253d6bed8e43f774a2
Mikael Trilles <11914687-mkaeltrilles@users.noreply.gitlab.com>
2022-07-01 11:44:14 +0200

    📦 NEW: mise en place cours

---
+++ app/Hub.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+class Hub
+{
+    private string $url;
+    private string $response;
+    private $curl;
+
+    public function __construct()
+    {
+        $this->curl = curl_init();
+    }
+
+    private function setCurlOptions()
+    {
+        curl_setopt_array($this->curl, [
+        CURLOPT_URL=>$this->url,
+        CURLOPT_CUSTOMREQUEST=>'GET',
+        CURLOPT_HTTPHEADER=>['Content-Type: application/json'],
+        CURLOPT_RETURNTRANSFER=>true,
+        ]);
+    }
+
+    private function execute()
+    {
+        $this->setCurlOptions();
+        $this->response = curl_exec($this->curl);
+        return json_decode($this->response);
+    }
+
+    public function getFormations()
+    {
+        $this->url = 'https://api-v2.hub3e.com/v2/ecoles/130/formations';
+        return $this->execute();
+    }
+}
